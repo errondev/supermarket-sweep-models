0013d2e5a47af77a92e86f306cb9a587af902fda
ntta <aaron.ntta@gmail.com>
2024-12-23 00:17:53 +1100

    chore: build for adding ts extensions

--- dist/enums.d.ts
+++
@@ -1,9 +0,0 @@
-export declare enum DayOfWeek {
-    Monday = "Monday",
-    Tuesday = "Tuesday",
-    Wednesday = "Wednesday",
-    Thursday = "Thursday",
-    Friday = "Friday",
-    Saturday = "Saturday",
-    Sunday = "Sunday"
-}
--- dist/types.d.ts
+++
@@ -1,1 +0,0 @@
-export type BestLocation = 'instore' | 'online' | 'both' | 'none';
